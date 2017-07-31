4a13b85e59e1c7c32f724758f89880a241909d43
Iangevin <Iannick.Langevin@gmail.com>
2017-07-31 13:23:49 -0400

    Test

--- Client/Phi-Box/Group.cs
+++ Client/Phi-Box/Group.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -20,6 +21,9 @@ namespace Phi_Box
         public uint adminId;
         public GroupStatus status;
 
+        FileSystemWatcher fsw;
+        string root = "C:/Phibox";
+
         public Group() { }
 
         public Group(uint _id, string _name, string _description, uint _adminId, GroupStatus _status)
@@ -30,6 +34,33 @@ namespace Phi_Box
             adminId = _adminId;
             status = _status;
             adminId = _adminId;
+
+            initFileWatcher();
+        }
+
+        private void initFileWatcher()
+        {
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+
+            if(!Directory.Exists(root + name))
+            {
+                Directory.CreateDirectory(root + name);
+            }
+
+            fsw = new FileSystemWatcher();
+            fsw.Path = root + name;
+            fsw.NotifyFilter = NotifyFilters.LastWrite;
+            fsw.Filter = "*.*";
+            fsw.Changed += new FileSystemEventHandler(OnChanged);
+            fsw.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+
         }
 
         public bool IsValid()
